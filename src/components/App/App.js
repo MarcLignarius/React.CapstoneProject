f2674a4f4e73ebacfdfcfc2073a53d1f993dd64e
Marc Davies <marcdaviesriot@gmail.com>
2019-07-26 15:34:43 -0700

    add helpers for adding, editing and deleting shows

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
-import { shows } from './../../data/data.js';
+import Header from '../header/header';
+import Show from '../show/show';
+import shows from './../../data/data.js';
 
-function App() {
-  return (
-    <div>
-      <p>This is the App component</p>
-    </div>
-  );
-}
+export default class App extends Component {
+  state={
+    shows,
+  }
 
-export default App;
+  getShows(){
+    return this.state.shows;
+  }
+
+  getShowIndex = (uid) => this.state.shows.findIndex((show) => show.uid === uid)
+
+  handleAddHype = (uid) => {
+    let updatedShows = [...this.state.shows];
+    updatedShows[this.getShowIndex(uid)].hypeScore = updatedShows[this.getShowIndex(uid)].hypeScore + 1;
+    this.setState({shows: updatedShows});
+  }
+
+  handleAddShow = (details) => {
+    let updatedShows = [...this.state.shows, details];
+    this.setState({shows: updatedShows});
+  }
+
+  handleEditShow = (details ) => {
+    let updatedShows = [...this.state.shows]
+    updatedShows[this.getShowIndex(details.uid)] = details;
+    this.setState({shows: updatedShows})
+  }
+
+  handleDeleteShow = (uid) => {
+    let updatedShows = [...this.state.shows];
+    updatedShows.splice(this.getShowIndex(uid), 1);
+    this.setState({shows: updatedShows});
+  }
+  render() {
+      return (
+        <div>
+          <Header />
+          <Show />
+        </div>
+      )
+    }
+  }
