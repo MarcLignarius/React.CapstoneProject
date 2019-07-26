dad7a068b5afb1274ae6891b931805cafc50a260
Marc Davies <marcdaviesriot@gmail.com>
2019-07-26 15:33:41 -0700

    add Header component

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div>
+      <p>This is the Header component</p>
+    </div>
+  );
+}
+
+export default Header;
